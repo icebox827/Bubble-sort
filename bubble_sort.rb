56825574bdff52414ca6acfb610112a9e82edb92
icebox827 <icebox827@gmail.com>
2020-12-29 11:27:55 -0500

    fix linters error

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,19 +1,19 @@
 def bubble_sort(arr)
-    for i in 0..arr.length
-        for j in i+1..arr.length-1
-            if arr[i] > arr[j]
-                arr[i], arr[j] = arr[j], arr[i]
-            end
-        end
+  for i in 0..arr.length
+    for j in i+1..arr.length-1
+      if arr[i] > arr[j]
+        arr[i], arr[j] = arr[j], arr[i]
+      end
     end
-    puts arr
+  end
+  puts arr
 end
 
 bubble_sort([15, 20, 3, 55, 4, 85, 9, 2, 11, 6, 14])
 
 def bubble_sort_by(arr)
-    arr.sort! {|firstI, secondI| firstI.upcase <=> secondI.upcase}
-    puts arr
+  arr.sort! {|firstI, secondI| firstI.upcase <=> secondI.upcase}
+  puts arr
 end
 
 bubble_sort_by(["Denis", "Stephen", "Eva", "Astride", "Britany"])
