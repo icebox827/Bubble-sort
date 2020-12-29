0b54e51cf18bc08678acf8fccc097538387595f9
icebox827 <icebox827@gmail.com>
2020-12-29 11:34:26 -0500

    Modify double quot to single quote

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,6 +1,6 @@
 def bubble_sort(arr)
   for i in 0..arr.length
-    for j in i+1..arr.length-1
+    for j in i + 1..arr.length - 1
       if arr[i] > arr[j]
         arr[i], arr[j] = arr[j], arr[i]
       end
@@ -12,8 +12,8 @@ end
 bubble_sort([15, 20, 3, 55, 4, 85, 9, 2, 11, 6, 14])
 
 def bubble_sort_by(arr)
-  arr.sort! {|firstI, secondI| firstI.upcase <=> secondI.upcase}
+  arr.sort! {|first_I, second_I| first_I.upcase <=> second_I.upcase}
   puts arr
 end
 
-bubble_sort_by(["Denis", "Stephen", "Eva", "Astride", "Britany"])
+bubble_sort_by(['Denis', 'Stephen', 'Eva', 'Astride', 'Britany'])
