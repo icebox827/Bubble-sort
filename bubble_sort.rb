03fb4d0ffbc51ba1a81b062ec7c43a6ab1822abf
icebox827 <icebox827@gmail.com>
2020-12-29 11:00:55 -0500

    define bubble_sort method

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,5 +1,14 @@
-def bubble_sort (list)
-
+def bubble_sort(arr)
+    swapped = false
+    for i in 0..arr.length
+        for j in i+1..arr.length-1
+            if arr[i] > arr[j]
+                arr[i], arr[j] = arr[j], arr[i]
+                swapped = true
+            end
+        end
+    end
+    puts arr
 end
 
-list = [5, 6, 8, 9, 3]
+bubble_sort([15, 20, 3, 55, 4, 85, 9, 2, 11, 6, 14])
