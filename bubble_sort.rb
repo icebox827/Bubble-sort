7216dd842e7cbf62f0679728a408dc7cdbfa3fdb
icebox827 <icebox827@gmail.com>
2020-12-30 11:49:44 -0500

    update both methods

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,22 +1,45 @@
 def bubble_sort(arr)
   arr.length.times do
     arr.each_with_index do |n, i|
-      if arr[i + 1]
-        next if n < arr[i + 1]
-          arr[i], arr[i + 1] = arr[i + 1], arr[i]
-        
-      end
+      next unless arr[i + 1]
+
+      next if n < arr[i + 1]
+
+      arr[i], arr[i + 1] = arr[i + 1], arr[i]
     end
   end
-  return arr
+
+  arr
 end
-  
+
 arr = [15, 20, 3, 55, 4, 85, 9, 2, 11, 6, 14]
+
 puts bubble_sort(arr)
 
 def bubble_sort_by(arr)
-  arr.sort! { |first_i, second_i| first_i.upcase <=> second_i.upcase }
+  counter = 0
+
+  while counter < array.length - 1
+
+    negative = yield(arr[counter], arr[counter + 1]).negative?
+
+    if !negative
+
+      arr[counter] = arr[counter + 1]
+
+      arr[counter + 1] = arr[counter]
+
+      counter = 0
+
+    else
+
+      counter += 1
+
+    end
+
+  end
+
+  p arr
 end
 
-arr = ['Denis', 'Stephen', 'Eva', 'Astride', 'Britany']
-puts bubble_sort_by(arr)
+p bubble_sort_by((%w[Denis Stephen Eva Astride Britany]) | left, right | left.length - right.length)
