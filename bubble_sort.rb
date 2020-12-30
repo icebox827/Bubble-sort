4512f08547d2a533aae92a09ee0acb236cacdf87
icebox827 <icebox827@gmail.com>
2020-12-30 07:01:55 -0500

    modify bubble_sort method review

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,19 +1,23 @@
 def bubble_sort(arr)
-  for i in 0..arr.length do
-    for j in i + 1..arr.length - 1 do
-      if arr[i] > arr[j]
-        arr[i], arr[j] = arr[j], arr[i]
+  arr.length().times do
+    arr.each_with_index do |n, i|
+      if arr[i + 1]
+        if n > arr[i + 1]
+          arr[i], arr[i + 1] = arr[i + 1], arr[i]
+        end
       end
     end
   end
   return arr
 end
-
-bubble_sort([15, 20, 3, 55, 4, 85, 9, 2, 11, 6, 14])
+  
+arr = [15, 20, 3, 55, 4, 85, 9, 2, 11, 6, 14]
+puts bubble_sort(arr)
 
 def bubble_sort_by(arr)
   arr.sort! { |first_i, second_i| first_i.upcase <=> second_i.upcase }
   return arr
 end
 
-bubble_sort_by(['Denis', 'Stephen', 'Eva', 'Astride', 'Britany'])
+arr = ['Denis', 'Stephen', 'Eva', 'Astride', 'Britany']
+puts bubble_sort_by(arr)
